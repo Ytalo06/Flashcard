afe21809cebab93fa55deda8abaea6729e83570b
Ytalo06 <00001114281712sp@al.educacao.sp.gov.br>
2024-11-27 23:21:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Educação Fisica',
+    'Cite 3 esporte que são em dupla?',
+    'voleibol, o tênis e o futevôlei'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo? ',
+    'Ásia'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o maior ditador do Brasil?',
+    'Humberto Castelo Branco'
+)
+
+criaCartao(
+    'Português',
+    'Quem escreveu a obra "Don Casmurro"',
+    'Machado de Assis'
+)
+
+criaCartao(
+    'Filosofia',
+    'Oque Platão denfendia?',
+    ' o Inatismo, nascemos como principios racionais e idèias inatas'
+)
+
+criaCartao(
+    'Português',
+    'Que ano começou o romantismo?',
+    '1770'
+)
